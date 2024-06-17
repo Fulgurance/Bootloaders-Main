d79d970912abe3a211dddfe7429630ae9ea81406
Fulgurance <zohran.londais@gmail.com>
2024-06-17 21:48:57 +0100

    Patched syntax error

--- Dracut/59.0.0/59.0.0.cr
+++ Dracut/59.0.0/59.0.0.cr
@@ -19,7 +19,7 @@ class Target < ISM::Software
     def prepareInstallation
         super
 
-        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install"],
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
                     path:       buildDirectoryPath)
 
         dracutconfData = <<-CODE
