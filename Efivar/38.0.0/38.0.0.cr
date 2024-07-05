148e2b9f3aeec83bfef28b6731b38176952c2f20
Fulgurance <zohran.londais@gmail.com>
2024-07-05 20:21:41 +0100

    Removed useless sub command

--- Efivar/38.0.0/38.0.0.cr
+++ Efivar/38.0.0/38.0.0.cr
@@ -10,7 +10,7 @@ class Target < ISM::Software
     def prepareInstallation
         super
 
-        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath} install LIBDIR=/usr/lib",
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath} install",
                     path:       buildDirectoryPath)
     end
 
